1b72b691c52d4f7b318c5e08a1d7fa0a18de9c7b
guusfelix2015 <gusfelix2015@gmail.com>
2024-11-06 00:11:13 -0300

    feat: criando repositorio sequelize de categoria

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,7 +1,19 @@
-import { IRepository, ISearchableRepository } from "../../shared/domain/repository/repository-interface";
+import { ISearchableRepository } from "../../shared/domain/repository/repository-interface";
+import { SearchParams } from "../../shared/domain/repository/search-params";
+import { SearchResult } from "../../shared/domain/repository/search-result";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
-export interface ICategoryRepository extends ISearchableRepository<Category, Uuid>   {
-  
-}
+export type CategoryFiler = string;
+
+export class CategorySearchParams extends SearchParams<CategoryFiler> { }
+
+export class CategorySearchResult extends SearchResult<Category> { }
+
+export interface ICategoryRepository
+  extends ISearchableRepository<
+    Category,
+    Uuid,
+    CategoryFiler,
+    CategorySearchParams
+  > { }
